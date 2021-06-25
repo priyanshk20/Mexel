18df2d829ab4c28980fa94b4586ecb6a791f3481
Priyansh Khandelwal <khandelwalpriyansh85@gmail.com>
2021-06-26 01:31:15 +0530

    Cells, FormulaBar added.

--- script.js
+++ script.js
@@ -17,11 +17,36 @@ $(document).ready(function () {
       }
     }
 
-    let column = $(`<div class="column-name colcod-${answer} colId-${i}" >${answer}</div>`);
-    $(".column-name-container").append(column);
+//     let column = $(`<div class="column-name colcod-${answer} colId-${i}" >${answer}</div>`);
+//     $(".column-name-container").append(column);
 
-    let row = $(`<div class="row-name" id="rowId-${i}">${i}</div>`);
-    $(".row-name-container").append(row);
-  }
+//     let row = $(`<div class="row-name" id="rowId-${i}">${i}</div>`);
+//     $(".row-name-container").append(row);
+//   }
+//   for (let i = 1; i <= 100; i++) {
+//     let row = $(`<div class="cell-row"></div>`);
+//     for (let j = 1; j <= 100; j++) {
+//         let colCode = $(`.colId-${j}`).attr("id").split("-")[1];
+//         let column = $(`<div class="input-cell" contenteditable="false" id = "row-${i}-col-${j}" data="code-${colCode}"></div>`);
+//         row.append(column);
+//     }
+//     $(".input-cell-container").append(row);
+// }
+
+let column = $(`<div class="column-name colId-${i}" id="colCod-${answer}">${answer}</div>`);
+$(".column-name-container").append(column);
+let row = $(`<div class="row-name" id="rowId-${i}">${i}</div>`);
+$(".row-name-container").append(row);
+}
+
+for (let i = 1; i <= 100; i++) {
+let row = $(`<div class="cell-row"></div>`);
+for (let j = 1; j <= 100; j++) {
+    let colCode = $(`.colId-${j}`).attr("id").split("-")[1];
+    let column = $(`<div class="input-cell" contenteditable="true" id = "row-${i}-col-${j}" data="code-${colCode}"></div>`);
+    row.append(column);
+}
+$(".input-cell-container").append(row);
+}
 
 });
